ce261d4cfce43a14a7c5338b17559a3e09322ea3
thehansguy <thehansguy@gmail.com>
2021-12-26 11:00:13 +0000

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const app = express();
 
 app.use(express.json());
 
+const checkForHaters = (req, res, next) => {
+  if (req.body.name === "Hater") {
+    res.json({
+      error: "Yo, we don't allow Haters here mehn!!",
+    });
+  } else {
+    next();
+  }
+};
+
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World",
   });
+});
+
+app.post("/", checkForHaters, (req, res) => {
+  res.send("You are logged in!");
 });
 
 app.listen(3000, () => {
